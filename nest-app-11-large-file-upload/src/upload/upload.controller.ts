02393be1424367793fcf370957f26f5759d1fe8e
ZZsimon <zz_simon@163.com>
2024-05-11 17:25:41 +0800

    upd

--- nest-app-11-large-file-upload/src/upload/upload.controller.ts
+++ nest-app-11-large-file-upload/src/upload/upload.controller.ts
@@ -8,6 +8,9 @@ import {
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { storage } from './storage';
 
+import { existsSync, mkdirSync, cpSync, rmSync } from 'fs';
+import { join } from 'path';
+
 @Controller('upload')
 export class UploadController {
   @Post()
@@ -22,5 +25,27 @@ export class UploadController {
   ) {
     console.log('body----------: ', body);
     console.log('file----------: ', files);
+
+    // .表示除了回车符和换行符以外的所有字符
+    // +表示至少一次
+
+    // \_表示匹配到“_”这个下划符
+    // \d表示[0-9]
+    // +表示至少一次
+
+    // match会返回所有匹配到的字符串，是一个数组
+    // 第二个元素是小括号()中匹配到的内容
+    const fileName = body.name.match(/(.+)\_\d+$/)[1];
+    const chunkDir = join(
+      __dirname,
+      '../../',
+      'uploads/chunks---------------' + fileName,
+    );
+
+    if (!existsSync(chunkDir)) {
+      mkdirSync(chunkDir);
+    }
+    cpSync(files[0].path, chunkDir + '/' + body.name);
+    rmSync(files[0].path);
   }
 }
---
+++ nest-app-11-large-file-upload/test.js
@@ -0,0 +1,14 @@
+// var str = "c-delay-report.exe_2"
+
+// const fileName = str.match(/(.+)\_(\d+)/);
+
+// console.log(fileName, 'fileName');
+// console.log(fileName.length, 'length');
+
+
+const res = Math.random()
+console.log(res, 'Math.random()');
+console.log(res.toString(), 'Math.random()');
+console.log(res.toString().slice(2, 12), 'Math.random()');
+
+
